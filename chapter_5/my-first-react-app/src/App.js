44748e207043143c8e1afb9407725fb165ed1eea
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-14 00:41:29 +0200

    chapter 5

---
+++ chapter_5/my-first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Rating from './Rating';
+import Products from './Products'
+
+function App () {
+  return (
+    <div className='App'>
+      <Products />
+    </div>
+  )
+}
+export default App;
