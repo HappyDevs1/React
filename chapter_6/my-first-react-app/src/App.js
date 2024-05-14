bb53423c7572a76ed839bd9d9ef4b964ecb17385
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-15 00:00:36 +0200

    finishing chapter 6

--- chapter_6/my-first-react-app/src/App.js
+++ chapter_6/my-first-react-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import JumboTronComponent from './JumboTronComponent';
+import UserForm from './UserForm';
 
 class App extends Component {
   render () {
     return (
       <div>
-        <JumboTronComponent />
+        <UserForm />
       </div>
     )
   }
