b7b2ebeb75dda5d32c2cd4be2d0af53fab37d64b
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-15 17:19:25 +0200

    minor changes

--- chapter_8/my-first-react-app/src/App.js
+++ chapter_8/my-first-react-app/src/App.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
-import GitHub from './GitHub'
-
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <GitHub />
-      </div>
-    )
+ 
+import React, { Component } from 'react'; 
+import GitHub from './GitHub'; 
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
+ 
+class App extends Component {        
+  render() {         
+    return ( 
+      <div> 
+        <Header /> 
+      </div> 
+    ); 
+  } 
+} 
+export default App; 
+ 
+class Header extends Component { 
+  render(){ 
+    return ( 
+        <BrowserRouter> 
+          <div>                    
+            <Switch>               
+              <Route path="/github" component={GitHub} />               
+              <Route exact path="/" component={Home} />              
+              <Route path="/*" component={NotFound} />                        
+            </Switch>    
+          </div>   
+        </BrowserRouter>               
+    ) 
+  } 
+} 
+ 
+class Home extends Component { 
+  render(){ 
+    return ( 
+      <div> 
+        Home         
+      </div> 
+    ) 
+  } 
+} 
+ 
+class NotFound extends Component { 
+  render(){
+    return <div>Not Found</div>
   }
-}
-
-export default App;
+} 
