bf8681f8ac84a19b2e72fe42ef37db83d906b8cf
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-15 11:42:25 +0200

    dynamic search

--- chapter_7/my-first-react-app/src/GitHub.js
+++ chapter_7/my-first-react-app/src/GitHub.js
@@ -1,34 +1,62 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { Media, Form, Button } from 'react-bootstrap';
 
 class GitHub extends Component {
   constructor() {
     super();
     this.state = {
-      isLoading : true
+      data: [],
+      searchTerm: '',
+      isLoading: false
     };
+    this.handleChange = this.handleChange.bind(this);   
+        this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     this.getGitHubData('greg')
   }
-  getGitHubData(_searchTerm){                 
-    axios.get("https://api.github.com/search/users?q="+_searchTerm) 
-    .then(res => {                                    
-    this.setState({ 
-    isLoading : false,                   
-    })                             
-    console.log(res.data.items);
-    });
+  getGitHubData(_searchTerm) {
+    axios.get("https://api.github.com/search/users?q=" + _searchTerm)
+      .then(res => {
+        this.setState({
+          isLoading: false,
+          data: res.data.items
+        })
+        console.log(res.data.items);
+      });
 
-    }
-    render () {
-      return (
-        <div>
-          { this.state.isLoading &&
-          <h4>Getting data...</h4>}
-        </div>
-      )
-    }
+  }
+  render() {
+    const listUsers = this.state.data.map((user) =>
+      <Media key={user.id}>
+        <a href={user.html_url}>
+          <img
+            width={64}
+            height={64}
+            className="mr-3"
+            src={user.avatar_url}
+            alt="Generic placeholder"
+          />
+        </a>
+        <Media.Body>
+          <h5>Login: {user.login}</h5>
+          <p>Id: {user.id}</p>
+        </Media.Body>
+      </Media>
+    );
+
+    return (
+      <div>
+        <h3>GitHub Users Results</h3>
+        {this.state.isLoading &&
+          <ReactLoading type="spinningBubbles" color="#444" />
+        }
+        {listUsers}
+      </div>
+    );
+  }
 }
 
 export default GitHub;
