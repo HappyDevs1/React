4329be2906e0694f802f56fc6e252c4f0654ccb7
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-22 11:32:52 +0200

    adding routes

--- chapter_9/reactcrud/src/App.js
+++ chapter_9/reactcrud/src/App.js
@@ -3,7 +3,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import User from './User'
+import User from './User';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 
 class App extends Component { 
 constructor(){ 
@@ -12,10 +13,24 @@ console.log(firebase);
 } 
 render() { 
 return ( 
-<div> 
-  <User />
+<div>
+<BrowserRouter> 
+<div>                             
+<Switch>                                          
+<Route exact path="/" component={User} />              
+<Route path="/*" component={NotFound} />                        
+</Switch>    
+</div>   
+</BrowserRouter>
 </div> 
 );
 }
 }
+
+class NotFound extends Component {
+  render() {
+    return <div>Not Found</div>
+  }
+}
+
 export default App;
