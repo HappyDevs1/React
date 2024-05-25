7d285a7073693ab015fd8b71ce56fbd6ee7c1042
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-25 23:56:58 +0200

    adding app content

--- chapter_12/hello_hooks/src/App.js
+++ chapter_12/hello_hooks/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  const [requested, setRequested] = useState(postsUrl);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="link" onClick={() => setRequested(postsUrl)}>
+        Beginning React Posts
+      </Button>
+      <Button variant="link" onClick={() => setRequested(todosUrl)}>
+        Todos
+      </Button>
+      <br />
+      Requested: {requested}
     </div>
   );
-}
-
+};
 export default App;
