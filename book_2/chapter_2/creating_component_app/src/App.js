e8370bfa99535c29e95deb88c2ba6045390cc2b1
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-27 12:34:20 +0200

    completing chapter 2

---
+++ book_2/chapter_2/creating_component_app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Products from './Products'
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName
+}
+function App() {
+  const user = {
+    firstName: "Happy",
+    lastName: "Mahlangu",
+    imageUrl: "https://picsum.photos/200/300"
+  }
+  return (
+    <div>
+      <h1>
+        Hello, {formatName(user)}
+        <br />
+        <img src={user.imageUrl}></img>
+      </h1>
+      <Products />
+    </div>
+  )
+}
+
+export default App
