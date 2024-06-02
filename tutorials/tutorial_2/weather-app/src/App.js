8250d4ab946208704680d5137b5a76a1ccbe7c48
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-06-03 01:59:24 +0200

    tutorial 2

---
+++ tutorials/tutorial_2/weather-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import WeatherApp from './weather/WeatherApp';
+
+function App () {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  )
+}
+
+export default App;
