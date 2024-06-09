636178c6e0153044da6471a408522624abee8048
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-06-09 23:54:07 +0200

    passing data

--- tutorials/tutorial_5/react-18-shopping-cart/src/App.js
+++ tutorials/tutorial_5/react-18-shopping-cart/src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header';
+import Main from './components/Main';
+import Basket from './components/Basket';
+import data from './data'
 
 function App() {
+  const { products } = data;
   return (
     <div>
       <Header />
-      <div>
-        <Main />
+      <div className='row'>
+        <Main products={products} />
         <Basket />
       </div>
     </div>
--- tutorials/tutorial_5/react-18-shopping-cart/src/components/Basket.js
+++ tutorials/tutorial_5/react-18-shopping-cart/src/components/Basket.js
@@ -1,3 +1,3 @@
 export default function Basket() {
-  return "Basket"
+  return <div className="block col-1">Basket</div>
 }
