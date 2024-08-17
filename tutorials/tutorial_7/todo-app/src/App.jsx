4760547c10a73288371cfca4f3682640274a55d8
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-18 01:12:46 +0200

    feat: :sparkles: creating the navigation bar for the todo app and adding tailwind for styling

--- tutorials/tutorial_7/todo-app/src/App.jsx
+++ tutorials/tutorial_7/todo-app/src/App.jsx
@@ -1,9 +1,44 @@
-function App () {
+import React, { useState } from "react";
+
+function App() {
+  const [auth, setAuth] = useState(false);
+
+  const login = () => {
+    setAuth(true);
+  };
+
+  const logout = () => {
+    setAuth(false);
+  };
   return (
-    <div>
-      <h1>My App component</h1>
-    </div>
-  )
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">Todo App</div>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#" className="text-white hover:text-gray-200">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-200">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-200">
+              Login
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-200">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default App;
