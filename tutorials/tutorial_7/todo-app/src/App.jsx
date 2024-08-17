99e4c9c68c9ddcca44b8cf411fdc785e0303984a
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-08-18 01:55:05 +0200

    feat: :sparkles: implementing routing

--- tutorials/tutorial_7/todo-app/src/App.jsx
+++ tutorials/tutorial_7/todo-app/src/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [auth, setAuth] = useState(false);
@@ -7,37 +13,38 @@ function App() {
     setAuth(true);
   };
 
-  const logout = () => {
+  const logout = () => { 
     setAuth(false);
   };
+
   return (
-    <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">Todo App</div>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="#" className="text-white hover:text-gray-200">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:text-gray-200">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:text-gray-200">
-              Login
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white hover:text-gray-200">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Router>
+      <nav className="bg-blue-500 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white text-lg font-bold">Todo App</div>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className="text-white hover:text-gray-200">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="text-white hover:text-gray-200">
+                {auth === true? "logout": "login"}
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/todo-list" element={<TodoList />} />
+      </Routes>
+    </Router>
   );
 }
 
